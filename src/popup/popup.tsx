7dce7cbef3a42297b10ed24ace0a2f79fe8c5b9f
mgsrevolver <clayreimus@gmail.com>
2022-03-28 18:05:31 -0400

    return temperature in toronto to console

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import './popup.css'
+import { fetchOpenWeatherData } from '../utils/api'
 
 const App: React.FC<{}> = () => {
+  useEffect(() => {
+    fetchOpenWeatherData('Toronto')
+      .then((data) => {
+        console.log(data)
+        console.log('Temperature is: ', data.main.temp)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
   return (
     <div>
       <img src="icon.png" />
