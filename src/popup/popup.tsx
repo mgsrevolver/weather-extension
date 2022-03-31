b6c3a81309650ad1a636277bc8d43d172ece99e2
mgsrevolver <clayreimus@gmail.com>
2022-03-30 20:11:26 -0400

    added celsius/farenheight toggle, but it does nothing yet

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,18 +1,33 @@
 import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
-import { Box, Grid, InputBase, IconButton, Paper } from '@material-ui/core'
+import {
+  Box,
+  Grid,
+  InputBase,
+  IconButton,
+  Paper,
+  Icon,
+} from '@material-ui/core'
 import { Add as AddIcon, ReportRounded } from '@material-ui/icons'
 import 'fontsource-roboto'
 import './popup.css'
 import WeatherCard from './WeatherCard'
-import { setStoredCities, getStoredCities } from '../utils/storage'
+import {
+  setStoredCities,
+  setStoredOptions,
+  getStoredOptions,
+  getStoredCities,
+  LocalStorageOptions,
+} from '../utils/storage'
 
 const App: React.FC<{}> = () => {
   const [cities, setCities] = useState<string[]>([])
   const [cityInput, setCityInput] = useState<string>('')
+  const [options, setOptions] = useState<LocalStorageOptions | null>(null)
 
   useEffect(() => {
     getStoredCities().then((cities) => setCities(cities))
+    getStoredOptions().then((options) => setOptions(options))
   }, [])
 
   const handleCityButtonClick = () => {
@@ -34,9 +49,13 @@ const App: React.FC<{}> = () => {
     })
   }
 
+  if (!options) {
+    return null
+  }
+
   return (
     <Box mx="8px" my="16px">
-      <Grid container>
+      <Grid container justifyContent="space-evenly">
         <Grid item>
           <Paper>
             <Box px="15px" py="5px">
@@ -50,6 +69,15 @@ const App: React.FC<{}> = () => {
               </IconButton>
             </Box>
           </Paper>
+          <Grid item>
+            <Paper>
+              <Box px="4px" py="4px">
+                <IconButton>
+                  {options.tempScale === 'metric' ? '\u2103' : '\u2109'}
+                </IconButton>
+              </Box>
+            </Paper>
+          </Grid>
         </Grid>
       </Grid>
 
