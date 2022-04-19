8288ba0281b78a5d5e113022332c8c929b46f790
mgsrevolver <clayreimus@gmail.com>
2022-04-19 19:15:58 -0400

    updated overlay to reference options page

--- src/contentScript/contentScript.tsx
+++ src/contentScript/contentScript.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
 import { Card } from '@material-ui/core'
 import WeatherCard from '../components/WeatherCard'
+import { getStoredOptions, LocalStorageOptions } from '../utils/storage'
 import './contentScript.css'
 
 const App: React.FC<{}> = () => {
+  const [options, setOptions] = useState<LocalStorageOptions | null>(null)
+
+  useEffect(() => {
+    getStoredOptions().then((options) => setOptions(options))
+  }, [])
+
+  if (!options) {
+    return null
+  }
+
   return (
     <Card className="overlayCard">
-      <WeatherCard city="New York" tempScale="imperial" />
+      <WeatherCard city={options.homeCity} tempScale={options.tempScale} />
     </Card>
   )
 }
